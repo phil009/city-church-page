f039ab7a5c7af4ca4b4a8beade69d7ed7a021500
phil009 <philipogboh60@gmail.com>
2025-06-21 12:06:06 +0100

    added new small group info

--- data/small-groups-data.ts
+++ data/small-groups-data.ts
@@ -1,115 +1,163 @@
 export const smallGroups = [
   {
     id: 1,
-    name: "Faith Foundry",
-    leaderName: "Benjamin Ekpe",
-    leaderStory:
-      "I want to help believers grow in the faith because once upon an experience, life's struggles made me question God's promises for my life.",
-    meetingDay: "Wednesdays",
-    meetingTime: "6:00 PM",
-    location:
-      "8, Ekpo Eyo Street, Off Uwanse (Police Station Junction), Calabar.",
-    vision:
-      "To build a community of believers who love God, face life's challenges and experience an uncommon growth. ",
-    description:
-      "We help each other experience personal and professional growth. We support each other to face daily challenges using hope and faith and celebrate each other's success..",
+    name: "Group 1",
+    leaderName: "Itafu Ogar",
+    leadersNumber: "08163868611",
+    location: "No. 4, Okon Inok Layout ",
+    landmark:
+      "Fiesta Fries is a major landmark. The house is after Vanel Hotel.",
+    whatsAppLink: "#",
   },
   {
     id: 2,
-    name: "Honeycomb",
-    leaderName: "Adebayo Asorose & Esther Etim",
-    leaderStory:
-      "We want better marriages . So we decided to start with ours. For the past months we have seen changes in our homes and we want others to experience these positive changes. Most couples will not ordinarily be intentional  or talk about details.  But this group has the potential to allow that",
-    meetingDay: "Thursdays",
-    meetingTime: "6:00 PM",
-    location: "13/14 David W. Noah street, Ekorinim, Calabar",
-    vision:
-      "To create a safe space where couples can have healthy conversations, build Godly marriages and improve their wolk with God.",
-    description:
-      "Hi, we’re Adebayo and Joy — married for 13 years+, blessed with a son and a daughter. We love God, we love people, and we deeply love marriages that thrive.There’s something truly beautiful about seeing couples still holding hands in their 80s, 90s, or even a hundred. That’s the kind of legacy we want. But let’s be real — good marriages don’t just happen. They take intentionality, commitment, and a shared desire to grow. One of the best things we've done for our marriage recently is partnering with another couple we admire to become what we now call 'Accountability Couples'. It's not your typical 'accountability partner' setup — this is two couples showing up for each other, committed to learning, growing, and holding space for honest conversations. Every week, we meet to share openly, learn from each other’s experiences, and challenge ourselves to love more deeply and intentionally. It’s been eye-opening, healing, and truly rewarding. And now, we want to extend the invite: If you're passionate about seeing your marriage not just survive, but flourish, and you’d love to grow in a safe, supportive space with other couples — then this might just be for you. We’re building a community of intentional couples who are doing the work — together. And we’d love to have you join us on this journey. Let’s grow together. Let’s thrive — not just for now, but for the long haul.",
+    name: "Group 2",
+    leaderName: "Ikezahu Adoghe",
+    leadersNumber: "07030585817",
+    location: "Udiba Crescent, Asari Eso Layout, Calabar",
+    landmark:
+      "Locate Angelseyes Academy. The house is after Angeleyes Academy, Asari Eso.",
+    whatsAppLink: "#",
   },
   {
     id: 3,
-    name: "Giant Slayers",
-    leaderName: "Itafu Olom Ogar",
-    leaderStory:
-      "Too often I have seen people begin in church and end up using their gifts cultivated in church to serve the world or serve self or serve the enemy wittingly or unwittingly. Most forget I John 3:15-17 admonition that we should not love the world. They face Giants in the mould of corruption, validation, riches, influence, leadership deficit etc. In Nigeria today there are many giants and instead of seeing them as challenges we have to see them as opportunity for greatness. This is a group meant to practically apply the word to everyday life to equip young professionals and semi professionals to take on giants and win like David did with his mighty men.",
-    meetingDay: "Wednesdays",
-    meetingTime: "6:00 PM",
-    location: "Church outside the big tent",
-    vision:
-      "To strengthen the faith and mindset of young people to be effective change agents with the Dominion mandate in the market place. ",
-    description:
-      "Our group is unique because we aim to build maximum spiritual and physical growth that positions our members to achieve great feats which we call giant slaying! In so doing we shall break bread, pray fervently and consistently, share ideas and execute on bold audacious plans in strict alignment with the word of God especially in the marketplace.",
+    name: "Group 3",
+    leaderName: "Emmanuel Obeten",
+    leadersNumber: "08063406654",
+    location: "Plot 13 Duke Henshaw Layout,Atimbo Road ",
+    landmark: "The house opposite former Jacaranda Suites.",
+    whatsAppLink: "#",
   },
   {
     id: 4,
-    name: "The Refuge",
-    leaderName: "Emmanuel Obeten",
-    leaderStory:
-      "I imagine a church where no one walks alone—where we build family, lasting friendships, and strong support systems in our journey towards spiritual transformation beyond just Sunday gatherings. I desire where individuals transition from just a nameless face to people of relevance,impact, change -fulfilling purpose within and beyond the church space.",
-    meetingDay: "Wednesdays",
-    meetingTime: "6:00 PM",
-    location: "Atimbo Road, Calabar",
-    vision:
-      "The Purpose to nurture true spiritual transformation through the Word, relationships, and accountability for lasting impact.",
-    description:
-      "City Church Calabar is more than just a church—it’s an amazing family, big enough to accommodate everyone, yet small enough to know you personally. Imagine that you don't ever have to do life alone! You don't have to walk alone. At THE REFUGE we build family, lasting friendships, and strong support systems in our journey towards spiritual transformation beyond just Sunday gatherings. These are powerful possibilities you can make your reality by joining our small group to participate as we anticipate the best God has for us. Join us at The Refuge, every Wednesdays by 6PM. It’s a place of his presence and Joyful relaxation. We’ll be expecting you!",
+    name: "Group 4",
+    leaderName: "Ani Etim Ani",
+    leadersNumber: "08033909260",
+    location: "No. 125B, Old Odukpani Road",
+    landmark:
+      "Opp. the new MUSBON Filling Station, close to flour mills junction, Ikot Ishie.",
+    whatsAppLink: "#",
   },
   {
     id: 5,
-    name: "The Love Lens",
-    leaderName: "Ini-Abasi Okulaja",
-    leaderStory:
-      "The overwhelming love of God transformed me into a lover of all and sundry… the unloved, the unloving, the unlovable and the loving . I want to share this love to my world.",
-    meetingDay: "Wednesdays",
-    meetingTime: "6:00 PM",
-    location: "Kids rock",
-    vision:
-      "A group where every person; loved, unloved, unloving and unlovable experiences the life changing power of God’s love and learn to extend that love to others building a family of grace, acceptance and transformation.",
-    description:
-      "Have you ever found yourself loving some people easily but struggling to love others? That’s me. I was selective with love until Jesus changed everything. He showed me that love is not about who deserves it but about who He is and became of Him, I now love freely ; the loving, the unloving, the unloved and the unlovable. That’s why I am starting this small group a place where we grow together and learn how to love like Jesus. If you are ready to experience love in a new way, to be a part of a group where love is overflowing, I’d love you to join us. Come and be a part of something life changing. Let’s love like Jesus… together! ",
+    name: "Group 5",
+    leaderName: "Promise Ndoma",
+    leadersNumber: "09060257949",
+    location: "No. 4, Diamond Lane, off Diamond Hill, Calabar",
+    landmark: "Winners Chapel Diamond Hill.",
+    whatsAppLink: "#",
   },
   {
     id: 6,
-    name: "God's Masterpiece",
-    leaderName: "John Ogah",
-    leaderStory:
-      "After saying 'yes' to Jesus over a decade ago, the presumed '360 transformation' didn't happen. I wandered, got entangled in choices that dimmed the light within me. I knew of God, but I didn't walk so close to His heart, carrying guilt like a shadow, convinced I had strayed too far. But mercy has a way of finding us in our mess. And in my lowest, He whispered love, not wrath. Grace didn’t just knock, it broke down walls I built in shame. The road of faith has not been smooth for me; it’s been a soul-battling terrain where my flaws tried to drown my fire, and doubt often danced too close. Yet even in my stumbling, Jesus stayed. He didn’t wait for perfection; He walked with me in my imperfection. He made each scar a story, each fall a lesson, and each tear a prayer. I’ve tasted both the ache of failure and the sweetness of redemption. And still, I rise, not because I have it all together, but because His love holds me together. Through it all, His faithfulness has remained my anchor, steady in every storm.",
-    meetingDay: "Wednesdays",
-    meetingTime: "6:00 PM",
-    location: "Church Reception",
-    vision:
-      "To blossom as a sacred space where faith takes root, grace meets every struggle, and each soul is seen, shaped, and celebrated as God’s masterpiece",
-    description: `
-"God's Masterpiece" is created to foster a warm and secure Christ-centered haven where hearts are nurtured in faith, challenges are embraced with courage and grace, and souls are lovingly supported through heartfelt prayers and mutual accountability.`,
+    name: "Group 6",
+    leaderName: "Adebayo Asorose ",
+    leadersNumber: "08032006026",
+    location: "13/14, David W. Noah street, Ekorinim Calabar",
+    landmark: "First Bank Ekorinim ",
+    whatsAppLink: "#",
   },
   {
     id: 7,
-    name: "Whole Bloom",
-    leaderName: "Favour Bassey",
-    leaderStory:
-      "Everyday, young women silently carry the weight of childhood trauma, rejection and abandonment which is often resulting from broken homes that should have been a place of love and safety. I know this pain because I have lived it. But I also know the power of God’s healing love to restore what was once shattered. 2Corinthians 1:3-5 NLT“He comforts you in all your troubles so you can comfort others when they are troubled, we will be able to give the same comfort God has given us. Through this ministry we are creating a Christ-centered healing space for young women to be seen, heard and made whole.",
-    meetingDay: "Wednesdays",
-    meetingTime: "6:00 PM",
-    location: "City Church Calabar",
-    vision:
-      "Restoring young women from broken homes to emotional, spiritual and relational wholeness through Christ-centered healing, mentorship and discipleship.",
-    description:
-      "This group is aimed to guide young women from survival (merely existing) to having an Identity (living), turning pain into purpose and brokenness into beauty; which is their identity in Christ Jesus. Hence equipping them to live whole, healed and purpose driven lives.",
+    name: "Group 7",
+    leaderName: "Kingsley Undiandeye ",
+    leadersNumber: "08062623660",
+    location: "34, Uwanse Street Calabar ",
+    landmark: "Deeper Life Church ",
+    whatsAppLink: "#",
   },
   {
     id: 8,
-    name: "Young & Purpose-Driven",
-    leaderName: "Kingsley Undiandeye",
-    leaderStory:
-      "I've noticed that life for many undergraduates especially can be daunting - balancing academic expectations with societal expectations can leave one flattened out. What's more, I faced this challenge in my time as a student & after school I thought it wise to help people in this phase of life.",
-    meetingDay: "Wednesdays",
-    meetingTime: "6:00 PM",
-    location: "The Big Tent, 98 Marian Road, Calabar",
-    vision:
-      "To raise a community of vibrant & spiritually inclined individuals who continually strive to improve on themselves in spite of obvious challenges. ",
-    description:
-      "This group is poised to help you understand that you shouldn't go through life without support systems. In this group you'll find an array of persons & resources designed to help your life become more purpose-driven.",
+    name: "Group 8",
+    leaderName: "Daniel Kanu ",
+    leadersNumber: "09034427647",
+    location:
+      "Engr. Edet Street, off Jehovah's Witness Road, Akai Efa/MCC, Calabar ",
+    landmark: "Opposite Orange Resort",
+    whatsAppLink: "#",
+  },
+  {
+    id: 9,
+    name: "Group 9",
+    leaderName: "Hannah Kanu",
+    leadersNumber: "08038889028",
+    location: "No. 2, Aqua Edem Crescent, Ekorinim, Calabar",
+    landmark:
+      "Behind Redeemed Christian Church of God (RCCG), Flourishing Parish. Its a straight drive from First Bank Highway.",
+    whatsAppLink: "#",
+  },
+  {
+    id: 10,
+    name: "Group 10",
+    leaderName: "Victor Eboigbe ",
+    leadersNumber: "09036572100",
+    location: "No. 3, Kadana Street, State Housing",
+    landmark:
+      "Access High School, State Housing or Event 45 by Douglas Avenue. ",
+    whatsAppLink: "#",
+  },
+  {
+    id: 11,
+    name: "Group 11",
+    leaderName: "Uma Ukpai",
+    leadersNumber: "08060532946",
+    location: "24, Atimbo Road, Calabar ",
+    landmark: "In-between Grand Hotel and A. A. Rano Filling Station.",
+    whatsAppLink: "#",
+  },
+  {
+    id: 12,
+    name: "Group 12",
+    leaderName: "Miracle Elijah",
+    leadersNumber: "08060967455",
+    location:
+      "No. 9 Pst. Charles Asuquo Street, Behind Uddy King Filling Station., Parliamentary Extension ",
+    landmark: "Uddy King Filling Station, Parliamentary Extension.",
+    whatsAppLink: "#",
+  },
+  {
+    id: 13,
+    name: "Group 13",
+    leaderName: "Precious Bassey",
+    leadersNumber: "08136006750",
+    location: "No. 5, Okon Effanga Street, Ikot Effanga, Calabar",
+    landmark: "The street opposite the WAEC Office on the Highway.",
+    whatsAppLink: "#",
+  },
+  {
+    id: 14,
+    name: "Group 14",
+    leaderName: "Benjamin Ekpe",
+    leadersNumber: "08038878602",
+    location: "No. 8, Ekpo Eyo Street, Calabar ",
+    landmark:
+      "Banner of God's Grace Headquarters. Ekpo Eyo is off Atamunu or Uwanse.",
+    whatsAppLink: "#",
+  },
+  {
+    id: 15,
+    name: "Group 15",
+    leaderName: "Collins Igajah",
+    leadersNumber: "08039344396",
+    location: "No. 69, Ibom Layout, by Mobil Filling Station",
+    landmark: "",
+    whatsAppLink: "#",
+  },
+  {
+    id: 16,
+    name: "Group 16",
+    leaderName: "Ini-Abasi Okulaja ",
+    leadersNumber: "08037252417",
+    location: "NextGen Hall",
+    landmark: "",
+    whatsAppLink: "#",
+  },
+  {
+    id: 17,
+    name: "Group 17",
+    leaderName: "John Ogah",
+    leadersNumber: "09078560384",
+    location: "City Church Reception",
+    landmark: "",
+    whatsAppLink: "#",
   },
 ];
