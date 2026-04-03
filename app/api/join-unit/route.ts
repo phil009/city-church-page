f88695b78fb2beb76b0de3d312304414ec8ced56
phil009 <philipogboh60@gmail.com>
2026-04-03 15:24:09 +0100

    updated ministers form

--- app/api/join-unit/route.ts
+++ app/api/join-unit/route.ts
@@ -20,6 +20,7 @@ export async function POST(request: NextRequest) {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
+                    typecast: true,
                     records: [
                         {
                             fields: body,
